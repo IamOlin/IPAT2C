5cec0584515b9b7e9ce365ec113ec16b7a40f067
IamOlin <marquez.og.bsinfotech@gmail.com>
2024-05-04 13:19:00 +0800

    Working Login and Logout with Display Data

--- IPAT2C/dcrm/models.py
+++ IPAT2C/dcrm/models.py
@@ -4,8 +4,6 @@ from django.db import models
 
 class Record(models.Model):
 
-
-
     created_at = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=50)
     last_name =  models.CharField(max_length=50)
@@ -18,3 +16,4 @@ class Record(models.Model):
 
     def __str__(self):
         return(f"{self.first_name} {self.last_name} {self.email}")
+
